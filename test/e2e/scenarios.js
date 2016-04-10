b6c81a9cc843a202c8ba46167019c614b5c38ddb
optimized4u <support@optimized4u.org>
2016-04-10 22:04:07 +0100

    Added test for search results.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,28 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('Real Racing Telemetry Client App', function() {
 
-  beforeEach(function() {
-    browser.get('app/index.html');
+  describe('Car list view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
+
+
+    it('should filter the car list as a user types into the search box', function() {
+
+      var carList = element.all(by.repeater('car in cars'));
+      var query = element(by.model('query'));
+
+      expect(carList.count()).toBe(3);
+
+      query.sendKeys('focus');
+      expect(carList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('audi');
+      expect(carList.count()).toBe(1);
+    });
   });
-
 });
