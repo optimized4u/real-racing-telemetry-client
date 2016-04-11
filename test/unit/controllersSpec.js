550549e235f8cad4c6857a8baadf391c900773f9
optimized4u <support@optimized4u.org>
2016-04-11 19:15:28 +0100

    Now injects scope into each test.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,25 +5,25 @@ describe('Real Racing Telemetry Client controllers', function() {
 
   describe('carController', function(){
 
+    var scope, ctrl;
+
     beforeEach(module('rrtClientApp'));
 
-    it('should create "cars" model with 4 cars', inject(function($controller) {
-      var scope = {},
-          ctrl = $controller('carController', {$scope:scope});
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('carController', {$scope:scope});
+    }));
 
+    it('should create "cars" model with 4 cars', inject(function($controller) {
       expect(scope.cars.length).toBe(4);
     }));
 
     it('should create title', inject(function($controller) {
-      var scope = {},
-          ctrl = $controller('carController', {$scope:scope});
-
       expect(scope.title).toBe("Admin for Cars and Manufacturers.");
     }));
 
     it('should set the default value of orderProp model', function() {
       expect(scope.orderProp).toBe('model');
     });
-
   });
 });
