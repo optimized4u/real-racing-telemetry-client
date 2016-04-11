7314e21482a85d296e3c419573c13ea14ff8efd8
optimized4u <support@optimized4u.org>
2016-04-11 16:25:46 +0100

    Changed formatting to make the car data more readable.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,15 +5,11 @@
 var rrtClientApp = angular.module('rrtClientApp', []);
 
 rrtClientApp.controller('carController', function($scope) {
-  $scope.title = "Admin for Cars and Manufacturers."
+  $scope.title = 'Admin for Cars and Manufacturers.'
   $scope.cars = [
-    {'model': 'Focus RS',
-     'manufacturer': 'Ford'},
-    {'model': 'R8 V10',
-     'manufacturer': 'Audi'},
-    {'model': 'LaFerrari',
-     'manufacturer': 'Ferrari'},
-     { 'model': 'Fusion',
-       'manufacturer': 'Ford' }
+    { model: 'Focus RS', manufacturer: 'Ford' },
+    { model: 'R8 V10', manufacturer: 'Audi' },
+    { model: 'LaFerrari', manufacturer: 'Ferrari' },
+    { model: 'Fusion', manufacturer: 'Ford' },
   ];
 });
